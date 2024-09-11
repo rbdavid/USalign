27263aa8a5f0198152a521f9c78cd43b8c7ab6ab
Russell Davidson <rbdchemistry@gmail.com>
2024-09-11 11:45:12 -0400

    slowly pushing updates from last week that never got pushed; this one is standardizing the compilation of the preprocessing_tests.cpp code so that I'm sure that comp arguments aren't affecting the tests

--- tests/compile_cpp_tests.sh
+++ tests/compile_cpp_tests.sh
@@ -1,3 +1,10 @@
 
-g++ -O3 -ffast-math -Wall -std=c++20 -fvisibility=hidden -g -I /home/russ/Scripts/git/USalign preprocessing_tests.cpp -o preprocessing_tests #-static# -lm
+
+g++ -O3 -ffast-math -I /home/russ/Scripts/git/USalign preprocessing_tests.cpp -o preprocessing_tests
+
+## this is the compile line that I was using for the preprocessing_tests and 
+## similar to the pybind11 compilation line; but it differs from USalign's 
+## default Make script's compilation arguments. For sanity sake, going to revert
+## back to those commands.
+##g++ -O3 -ffast-math -Wall -std=c++20 -fvisibility=hidden -g -I /home/russ/Scripts/git/USalign preprocessing_tests.cpp -o preprocessing_tests #-static# -lm
 
