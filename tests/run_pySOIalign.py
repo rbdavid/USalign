2b5d272783c5710b69f1676980329decce9cc887
Russell Davidson <rbdchemistry@gmail.com>
2024-09-17 14:47:51 -0400

    implemented an enum version of the tri2single amino acid resname parser. why? cause I felt like it

--- tests/run_pySOIalign.py
+++ tests/run_pySOIalign.py
@@ -9,13 +9,14 @@
 
 import sys
 sys.path.insert(0,'/home/russ/Scripts/git/USalign/pybindings') 
-
+import enum
 import pySOIalign
 import numpy as np
 import MDAnalysis
 
-mm_opt = 6
 closeK_opt = 0
+mm_opt = 6
+
 
 def readPDB(filename):
     """
@@ -30,57 +31,157 @@ def readPDB(filename):
     
     coords = [[line[30:38].strip(),line[38:46].strip(),line[46:54].strip()] for line in ca_atom_lines]
     coords = np.array(coords, dtype = np.float64)
-    seq = ''.join([tri2single(line[16:20]) for line in ca_atom_lines])
+    seq = ''.join([AAtri2single(line[16:20].strip().upper()) for line in ca_atom_lines])
 
     return coords, seq
 
 
-def tri2single(resname):
+class AAResnameMapping(enum.Enum):
+    """
+    only a subset of amino acid residue names understood by MDAnalysis:
+    https://userguide.mdanalysis.org/stable/standard_selections.html#protein-selection
+
+    !!! denotes special/ambiguous cases
+    """
+    # amino acids
+    ALA = 'A'
+    ARG = 'R'
+    ASN = 'N'
+    ASP = 'D'
+    ASX = 'B'   # !!!
+    CYS = 'C'
+    GLN = 'Q'
+    GLU = 'E'
+    GLX = 'Z'   # !!!
+    GLY = 'G'
+    HIS = 'H'   # !!!
+    HIE = 'H'   # !!!
+    HID = 'H'   # !!!
+    HIP = 'H'   # !!!
+    ILE = 'I'
+    LEU = 'L'
+    LYS = 'K'
+    MET = 'M'
+    PHE = 'F'
+    PRO = 'P'
+    SER = 'S'
+    THR = 'T'
+    TRP = 'W'
+    TYR = 'Y'
+    VAL = 'V'
+    UNK = 'X'   # !!!
+    
+
+class NAResnameMapping(enum.Enum):
+    """
+    only a subset of nucleic acid residue names understood by MDAnalysis:
+    https://userguide.mdanalysis.org/stable/standard_selections.html#nucleic-acids
+
+    !!! denotes special/ambiguous cases
+    """
+    # nucleic acids
+    ADE = 'A'
+    CYT = 'C'
+    GUA = 'G'
+    THY = 'T'
+    URA = 'U'
+    UNK = 'X'   # !!!
+
+
+def AAtri2single(resname):
     """
     CONVERT FROM 3 TO 1 LETTER AA CODES
     Read in an amino acid's three letter code and return the aa's single 
     letter code.
 
     Input:
-        resname: a string of length 3, any case
+        :param resname: a string.upper(), associated with a name in the 
+                        AAResnameMapping Enum object
     Output:
-        the single letter code of associated aa; if resname is unexpected, 
-        returns 'X' string.
+        :return: the single letter code for the associated aa. 
+                 If resname is unexpected, returns 'X' string to denote an 
+                 unknown resname. Or an empty string, if the Enum fails for an
+                 unexpected error.
     """
-    return {'ALA': 'A',
-            'ARG': 'R',
-            'ASN': 'N',
-            'ASP': 'D',
-            'CYS': 'C',
-            'GLN': 'Q',
-            'GLU': 'E',
-            'GLY': 'G',
-            'HIS': 'H',
-            'HIE': 'H',
-            'HID': 'H',
-            'ILE': 'I',
-            'LEU': 'L',
-            'LYS': 'K',
-            'MET': 'M',
-            'PHE': 'F',
-            'PRO': 'P',
-            'SER': 'S',
-            'THR': 'T',
-            'TRP': 'W',
-            'TYR': 'Y',
-            'VAL': 'V'}.get(resname.upper(), 'X')
+    try: 
+        return AAResnameMapping[resname].value
+    except KeyError:
+        print(f"{resname} not in the AAResnameMapping. Returning 'X' to denote an unknown residue type.")
+        return 'X'
+    except Exception as e:
+        print(f"AAtri2single({resname}) failed unexpectedly. Returning '' to remove this {resname} from the analysis.")
+        return ''
 
-#u = MDAnalysis.Universe(
-#        '/home/russ/Scripts/git/USalign/tests/data/2pel_chainA.pdb')
-#u_CAs = u.select_atoms('name CA')
-#u_len = u_CAs.n_atoms
-#u_CAs_coords = np.array(u_CAs.positions.flatten(),dtype=np.float64)
-#u_seq = ''.join([tri2single(a.resname) for a in u_CAs])
-u_CAs_coords, u_seq = readPDB('/home/russ/Scripts/git/USalign/tests/data/2pel_chainA.pdb')
-u_len = u_CAs_coords.shape[0]
+
+def AAsingle2tri(resname):
+    """
+    CONVERT FROM 1 TO 3 LETTER AA CODES
+    Read in an amino acid's single letter code and return the aa's tri letter
+    code.
+
+    !!! NOTE: There are ambiguous values in the AAResnameMapping Enum object.
+              Due to the organization of the Enum, the first entry with the 
+              value will be used.
+
+    Input:
+        :param resname: a string.upper(), associated with a value in the 
+                        AAResnameMapping Enum object
+    Output:
+        :return: the tri letter code for the associated aa. 
+                 If resname is unexpected, returns 'UNK' string to denote an 
+                 unknown resname. Or an empty string, if the Enum fails for an
+                 unexpected error.
+    """
+    try:
+        return AAResnameMapping(resname).name
+    except ValueError:
+        print(f"{resname} not in the AAResnameMapping. Returning 'UNK' to denote an unknown residue type.")
+        return 'UNK'
+    except Exception as e:
+        print(f"AAsingle2tri({resname}) failed unexpectedly. Returning '' to remove this {resname} from the analysis.")
+        return ''
+
+#
+#def tri2single(resname):
+#    """
+#    no real error handling was done here. just giving 'X' back if resname was 
+#    unexpected.
+#    """
+#    return {'ALA': 'A',
+#            'ARG': 'R',
+#            'ASN': 'N',
+#            'ASP': 'D',
+#            'CYS': 'C',
+#            'GLN': 'Q',
+#            'GLU': 'E',
+#            'GLY': 'G',
+#            'HIS': 'H',
+#            'HIE': 'H',
+#            'HID': 'H',
+#            'ILE': 'I',
+#            'LEU': 'L',
+#            'LYS': 'K',
+#            'MET': 'M',
+#            'PHE': 'F',
+#            'PRO': 'P',
+#            'SER': 'S',
+#            'THR': 'T',
+#            'TRP': 'W',
+#            'TYR': 'Y',
+#            'VAL': 'V'}.get(resname.upper(), 'X')
+#
+
+u = MDAnalysis.Universe(
+        '/home/russ/Scripts/git/USalign/tests/data/2pel_chainA.pdb')
+u_CAs = u.select_atoms('name CA')
+u_len = u_CAs.n_atoms
+u_CAs_coords = np.array(u_CAs.positions.flatten(),dtype=np.float64)
+u_seq = ''.join([AAtri2single(a.resname.upper()) for a in u_CAs])
+#u_CAs_coords, u_seq = readPDB('/home/russ/Scripts/git/USalign/tests/data/2pel_chainA.pdb')
+#u_len = u_CAs_coords.shape[0]
 u_sec = pySOIalign.make_sec_py(u_CAs_coords, u_len)
 u_sec_bonds = pySOIalign.assign_sec_bond_py(u_sec, u_len)
-u_neighbor_list = pySOIalign.getCloseK_py(u_CAs_coords, u_len, 5)
+u_neighbor_list = pySOIalign.getCloseK_py(u_CAs_coords, u_len, closeK_opt)
 u_alnStruct = pySOIalign.alnStruct(u_CAs_coords, 
                                    u_neighbor_list, 
                                    u_sec_bonds, 
@@ -88,17 +189,19 @@ u_alnStruct = pySOIalign.alnStruct(u_CAs_coords,
                                    u_sec, 
                                    u_len)
 
-#v = MDAnalysis.Universe(
-#        '/home/russ/Scripts/git/USalign/tests/data/3cna_chainA.pdb')
-#v_CAs = v.select_atoms('name CA')
-#v_CAs_coords = np.array(v_CAs.positions.flatten(),dtype=np.float64)
-#v_len = v_CAs.n_atoms
-#v_seq = ''.join([tri2single(a.resname) for a in v_CAs])
-v_CAs_coords, v_seq = readPDB('/home/russ/Scripts/git/USalign/tests/data/3cna_chainA.pdb')
-v_len = v_CAs_coords.shape[0]
+print(u_len, u_seq, u_len == len(u_seq), u_CAs_coords.shape)
+
+v = MDAnalysis.Universe(
+        '/home/russ/Scripts/git/USalign/tests/data/3cna_chainA.pdb')
+v_CAs = v.select_atoms('name CA')
+v_CAs_coords = np.array(v_CAs.positions.flatten(),dtype=np.float64)
+v_len = v_CAs.n_atoms
+v_seq = ''.join([AAtri2single(a.resname.upper()) for a in v_CAs])
+#v_CAs_coords, v_seq = readPDB('/home/russ/Scripts/git/USalign/tests/data/3cna_chainA.pdb')
+#v_len = v_CAs_coords.shape[0]
 v_sec = pySOIalign.make_sec_py(v_CAs_coords, v_len)
 v_sec_bonds = pySOIalign.assign_sec_bond_py(v_sec, v_len)
-v_neighbor_list = pySOIalign.getCloseK_py(v_CAs_coords, v_len, 5)
+v_neighbor_list = pySOIalign.getCloseK_py(v_CAs_coords, v_len, closeK_opt)
 v_alnStruct = pySOIalign.alnStruct(v_CAs_coords, 
                                    v_neighbor_list, 
                                    v_sec_bonds, 
@@ -106,7 +209,10 @@ v_alnStruct = pySOIalign.alnStruct(v_CAs_coords,
                                    v_sec, 
                                    v_len)
 
-uv_alnParameters = pySOIalign.alnParameters(0, -2, 6, 0, False, 0.0, False, 0.0, False)
+print(v_len, v_seq, v_len == len(v_seq), v_CAs_coords.shape)
+
+uv_alnParameters = pySOIalign.alnParameters(closeK_opt, -1*(u_len+v_len), mm_opt)
+#uv_alnParameters = pySOIalign.alnParameters(0, -1*(u_len+v_len), 6, 0, False, 0.0, False, 0.0, False)
 
 results = pySOIalign.runSOIalign(u_alnStruct, v_alnStruct, uv_alnParameters)
 
